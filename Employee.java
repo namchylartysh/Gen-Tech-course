45930fade5721505d2c010e861fda1402fac1140
namchylartysh <artyshnam@gmail.com>
2023-02-16 00:22:52 +0600

    task

---
+++ Employee.java
@@ -0,0 +1,15 @@
+package gentech;
+
+import java.util.HashMap;
+
+public class Employee {
+    public static void main(String[] args) {
+        HashMap<Integer, String> employees = new HashMap<>();
+
+
+        employees.put(01, "Лидия Аркадьевна Бубликова");
+        employees.put(02, "Иван Михайлович Серебряков");
+        System.out.println(employees);
+
+    }
+}
